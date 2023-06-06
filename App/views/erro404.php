96ab98d0d5d9ec35701ad484d68a9a1a31d628bd
Pedro Bianchini de Quadros <pedrobiqua@gmail.com>
2023-06-06 11:26:00 -0300

    Update erro404.php
    Remoção da imagem que não estava sendo utilizada

--- App/views/erro404.php
+++ App/views/erro404.php
@@ -1,7 +1,7 @@
 <div class="container">
   <div class="row">
     <div class="col-12 text-center" style="margin-top:200px">
-      <img src="/assets/img/erro.png">
+      ERRO 404
     </div>
   </div>
 </div>

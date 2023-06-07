0d00850876f8ec098ce4d998728802aa0fc98589
Gabriel Farias <gagf@protonmail.com>
2023-06-07 13:35:29 -0300

    Cabeçalho, paragrafo fica relativo a header, etc

--- App/views/home/index.php
+++ App/views/home/index.php
@@ -1,5 +1,6 @@
 <div class="container">
   <div class="d-flex align-items-center justify-content-center m-3">
     <h1 style="color: #00bf63;" class="fonteIBM">NutriAgente conectando nutricionistas e seus clientes</h1>
+    
   </div>
 </div>
--- Public/index.php
+++ Public/index.php
@@ -10,59 +10,23 @@
     <title>NutriAgente</title>
   </head>
   <body>
-  <div class="container">
-    <div class="corFundo">
-      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-        <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-dark text-decoration-none">
-          <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>
-        </a>
 
-        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-          <li><a href="home" class="nav-link px-2 link-secondary"><img src="../Public/assets/img/logosemarco.png" width="100" height="100"></a></li>
-        </ul>
-
-        <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-            <a href="/" class="d-flex align-items-center my-2 my-lg-0 me-lg-auto text-white text-decoration-none">
-              <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>
-            </a>
-
-            <ul class="nav col-12 col-lg-auto my-2 justify-content-center my-md-0 text-small">
-              <li>
-                <a href="#" class="nav-link text-white">
-                  <svg class="bi d-block mx-auto mb-1" width="24" height="24"><use xlink:href="#home"></use></svg>
-                  Home
-                </a>
-              </li>
-              <li>
-                <a href="#" class="nav-link text-white">
-                  <svg class="bi d-block mx-auto mb-1" width="24" height="24"><use xlink:href="#speedometer2"></use></svg>
-                  Dashboard
-                </a>
-              </li>
-              <li>
-                <a href="#" class="nav-link text-white">
-                  <svg class="bi d-block mx-auto mb-1" width="24" height="24"><use xlink:href="#table"></use></svg>
-                  Orders
-                </a>
-              </li>
-              <li>
-                <a href="#" class="nav-link text-white">
-                  <svg class="bi d-block mx-auto mb-1" width="24" height="24"><use xlink:href="#grid"></use></svg>
-                  Products
-                </a>
-              </li>
-              <li>
-                <a href="login" class="nav-link fonteDosis" style="color: #00bf63;">
-                  <svg class="bi d-block mx-auto mb-1" width="24" height="24"><use xlink:href="#people-circle"></use></svg>
-                  Login
-                </a>
-              </li>
-            </ul>
-          </div>
+  <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
+      <div class="col-md-3 mb-2 mb-md-0">
+      <li><a href="home" class="nav-link px-2 link-secondary"><img src="../Public/assets/img/logosemarco.png"></a></li>
       </div>
-    </div>
-  </div>
+
+
+
+      <div class="col-md-3 text-end">
+      <a href="login"><button type="button" class="btn btn-outline-primary botao me-2 verdeEscuro">Login</button></a>
+        <button type="button" class="btn o me-2 verdeEscuro"></button>
+        <button type="button" class="btn o me-2 verdeEscuro"></button>
+      </div>
+    </header>
   
+
+
   <?php
     require '../App/autoload.php';
 

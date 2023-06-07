7cd1c333169d9b052728f10e7a6781378c7d3777
Pedro Bianchini de Quadros <pedrobiqua@gmail.com>
2023-06-07 09:51:34 -0300

    Melhorias no Bootsrap e começo do cabeçalho

--- App/views/home/index.php
+++ App/views/home/index.php
@@ -1,5 +1,5 @@
-<div class="container">
-  <div style="margin:150px">
-    <h1>PÁGINA INICIAL - NUTRIAGENTE</h1>
+  <div class="container">
+  <div class="d-flex align-items-center justify-content-center m-3">
+    <h1 style="color: #00bf63;">NutriAgente conectando nutricionistas e seus clientes</h1>
   </div>
 </div>
--- Public/index.php
+++ Public/index.php
@@ -7,7 +7,57 @@
     
   </head>
   <body>
+  <div class="container">
+    <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+      <a href="/" class="d-flex align-items-center mb-2 mb-lg-0 text-dark text-decoration-none">
+        <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>
+      </a>
 
+      <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+        <li><a href="#" class="nav-link px-2 link-secondary"><img src="../Public/assets/img/logo.png" width="100" height="100"></a></li>
+      </ul>
+
+      <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+          <a href="/" class="d-flex align-items-center my-2 my-lg-0 me-lg-auto text-white text-decoration-none">
+            <svg class="bi me-2" width="40" height="32" role="img" aria-label="Bootstrap"><use xlink:href="#bootstrap"></use></svg>
+          </a>
+
+          <ul class="nav col-12 col-lg-auto my-2 justify-content-center my-md-0 text-small">
+            <li>
+              <a href="#" class="nav-link text-white">
+                <svg class="bi d-block mx-auto mb-1" width="24" height="24"><use xlink:href="#home"></use></svg>
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#" class="nav-link text-white">
+                <svg class="bi d-block mx-auto mb-1" width="24" height="24"><use xlink:href="#speedometer2"></use></svg>
+                Dashboard
+              </a>
+            </li>
+            <li>
+              <a href="#" class="nav-link text-white">
+                <svg class="bi d-block mx-auto mb-1" width="24" height="24"><use xlink:href="#table"></use></svg>
+                Orders
+              </a>
+            </li>
+            <li>
+              <a href="#" class="nav-link text-white">
+                <svg class="bi d-block mx-auto mb-1" width="24" height="24"><use xlink:href="#grid"></use></svg>
+                Products
+              </a>
+            </li>
+            <li>
+              <a href="#" class="nav-link" style="color: #00bf63;">
+                <svg class="bi d-block mx-auto mb-1" width="24" height="24"><use xlink:href="#people-circle"></use></svg>
+                Login
+              </a>
+            </li>
+          </ul>
+        </div>
+    </div>
+  </div>
+  
   <?php
     require '../App/autoload.php';
 

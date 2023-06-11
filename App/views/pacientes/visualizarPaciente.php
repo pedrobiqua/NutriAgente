9a4c90f7e87f8499ce83a6fda628648e1d4068c3
Gabriel Farias <gagf@protonmail.com>
2023-06-11 20:24:11 -0300

    visualizarPaciente criado, img gráfico

---
+++ App/views/pacientes/visualizarPaciente.php
@@ -0,0 +1,64 @@
+<div class="row mb-3 justify-content-center">
+    <div class="col-lg-5 mx-4">
+      <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative" style = "height:600px">
+        <div class="col p-4 d-flex flex-column position-static">
+        <h1 class="fonteIBM verdeEscuro" style="font-size:78px"><strong>Nome paciente</strong></h1>
+        <p></p>
+        <p></p>
+        <p></p>
+
+        <!-- Peso aqui -->
+        <h2 class="fonteDosis" style="font-size:72px">Peso:</h2>
+        <p></p>
+        <p></p>
+        <p></p>
+
+        <!-- Altura aqui -->
+        <h2 class="fonteDosis" style="font-size:72px">Altura:</h2>
+        <p></p>
+        <p></p>
+        <p></p>
+
+        <!-- IMC aqui -->
+        <h2 class="fonteDosis" style="font-size:72px">IMC:</h2>
+            </div>
+        </div>
+    </div>
+    
+
+        
+    </div>
+</div>
+<div class="row mb-3 justify-content-center">
+    <div class="col-lg-5 mx-4 justify-content-center items-align-center">
+      <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative " style = "height:600px">
+        <div class="col p-4 d-flex flex-column position-static ">
+        <h1 class="dfonteIBM verdeEscuro text-center" style="font-size:78px"> <strong>Consumo</strong></h1>
+        <img class = "rounded mx-auto d-block" alt="Consumo" src="../Public/assets/img/grafico.png" width = "800" height = "500"/>
+
+            </div>
+        </div>
+    </div>
+    
+
+        
+    </div>
+</div>
+<div class="row mb-3 justify-content-center">
+    <div class="col-lg-5 mx-4">
+      <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative" style = "height:600px">
+        <div class="col p-4 d-flex flex-column position-static">
+        <h1 class="dfonteIBM verdeEscuro text-center" style="font-size:72px"><strong>Plano</strong></h1>
+        <h2 class="fonteDosis" style="font-size:36px">Objetivo: Emagrecimento</h2>
+        <h2 class="fonteDosis" style="font-size:36px">Descrição: Fazer refeições ricas em proteinas, fazer exercícios semanalmente, é fundamental beber bastante água ao longo do dia, evitar alimentos processados e ricos em açúcar, controlar as porções e praticar exercícios físicos regularmente.</h2>
+            </div>
+        </div>
+    </div>
+    
+
+        
+    </div>
+</div>
+
+
+
